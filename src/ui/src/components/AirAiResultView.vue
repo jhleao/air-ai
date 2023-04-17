58262f044497cd267b4e89b36bd3538a97350f2a
José Henrique Leão <jhleao99@gmail.com>
2023-04-16 22:39:16 -0300

    feat: add result ui component and fetching logic

---
+++ src/ui/src/components/AirAiResultView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div>
+      <h5 class="title">Result</h5>
+      <p class="text">
+        {{ result.answer }}
+      </p>
+    </div>
+    <div v-if="result.facts?.length">
+      <h5 class="title">Facts</h5>
+      <p class="text">
+        <ul>
+          <li class="fact" v-for="fact in result.facts" :key="fact">{{ fact }}</li>
+        </ul>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { AirAiResult } from '@/util/schema';
+
+export default defineComponent({
+  props: {
+    result: {
+      type: AirAiResult,
+      required: true
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: 0 auto;
+  width: 100%;
+  max-width: 500px;
+  margin-top: 1rem;
+}
+
+.title {
+  font-size: 1.4rem;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+  color: $gray800;
+}
+
+.text {
+  font-size: 1rem;
+  line-height: 1.1rem;
+  font-weight: 400;
+  margin-bottom: 0.5rem;
+  color: $gray600;
+}
+
+.fact {
+  margin-bottom: 1rem;
+}
+</style>
--- src/ui/src/views/MainView.vue
+++ src/ui/src/views/MainView.vue
@@ -11,12 +11,19 @@
     <LoadingState v-if="view === View.Loading" />
     <ErrorState v-if="view === View.Error" @tryAgain="onSubmit" />
     <GreetingText v-if="view === View.Greeting" />
+    <AirAiResultView v-if="view === View.Result && result" :result="result" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
 import PromptInput from '@/components/PromptInput.vue';
+import { defineComponent, ref } from 'vue';
+import AirAiResultView from '../components/AirAiResultView.vue';
+import ErrorState from '../components/ErrorState.vue';
+import GreetingText from '../components/GreetingText.vue';
+import LoadingState from '../components/LoadingState.vue';
+import { AirAiApi } from '../util/api';
+import type { AirAiResult } from '../util/schema';
 
 enum View {
   Greeting = 'GREETING',
@@ -26,18 +33,30 @@ enum View {
 }
 
 export default defineComponent({
-  components: { PromptInput },
+  components: { PromptInput, LoadingState, GreetingText, AirAiResultView, ErrorState },
   setup() {
     const inputValue = ref('');
     const view = ref(View.Greeting);
+    const result = ref<AirAiResult | null>(null);
 
     const onInputChange = (value: string) => {
       inputValue.value = value;
     };
 
-    const onSubmit = async () => {};
+    const onSubmit = async () => {
+      if (!inputValue.value) return;
+      view.value = View.Loading;
+      const prompt = inputValue.value;
+      try {
+        const apiResponse = await AirAiApi.prompt(prompt);
+        result.value = apiResponse;
+        view.value = View.Result;
+      } catch {
+        view.value = View.Error;
+      }
+    };
 
-    return { inputValue, onInputChange, onSubmit, view, View };
+    return { inputValue, onInputChange, onSubmit, view, View, result };
   }
 });
 </script>
