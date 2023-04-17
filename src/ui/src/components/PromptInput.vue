975a2a77b0ad38dc0e265b81eed8b65568982887
José Henrique Leão <jhleao99@gmail.com>
2023-04-16 22:35:07 -0300

    feat: add controlled prompt input component

---
+++ src/ui/src/components/PromptInput.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="container">
+    <input
+      :value="value"
+      @input="onChange"
+      @keypress="onKeypress"
+      class="input"
+      v-bind="{ ...$attrs }"
+    />
+    <div class="divider"></div>
+    <button @click="submit" class="send" :class="{ disabled: submitDisabled }" type="button">
+      Send
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    value: {
+      type: String,
+      required: true
+    },
+    submitDisabled: {
+      type: Boolean,
+      required: false
+    }
+  },
+  methods: {
+    onChange(e: Event) {
+      this.$emit('update', e?.target?.value ?? '')
+    },
+    onKeypress(e: KeyboardEvent) {
+      if (e?.key === 'Enter') {
+        this.submit()
+      }
+    },
+    submit() {
+      if (this.submitDisabled) return
+      this.$emit('submit')
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+$height: 38px;
+$radius: 0.5rem;
+$border-thickness: 1px;
+
+.container {
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 500px;
+  margin-top: 1rem;
+  border: solid $border-thickness $gray300;
+  border-radius: $radius;
+  background: none;
+  height: $height;
+}
+
+.divider {
+  height: $height;
+  width: $border-thickness;
+  background-color: $gray300;
+}
+
+.input {
+  width: 100%;
+  height: 100%;
+  border: none;
+  outline: none;
+  font-size: 1.1rem;
+  color: $gray500;
+  margin: 0;
+  background: none;
+  padding: 0.5rem;
+  padding-left: 1rem;
+
+  &::placeholder {
+    color: $gray400;
+    font-size: 1.05rem;
+  }
+}
+
+.send {
+  font-size: 1.1rem;
+  color: $gray500;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  background: none;
+  border: none;
+  height: calc($height - $border-thickness * 2);
+  border-top-right-radius: $radius;
+  border-bottom-right-radius: $radius;
+  transition: background-color 0.1s ease-out;
+  z-index: 1;
+
+  &:hover {
+    background-color: $gray75;
+    cursor: pointer;
+  }
+}
+
+.disabled {
+  color: $gray300;
+  background-color: $gray75;
+  cursor: pointer;
+}
+</style>
--- src/ui/src/views/MainView.vue
+++ src/ui/src/views/MainView.vue
@@ -1,9 +1,49 @@
 <template>
- hi
+  <div class="main-container">
+    <PromptInput
+      :value="inputValue"
+      :submitDisabled="view === View.Loading"
+      @update="onInputChange"
+      @submit="onSubmit"
+      placeholder="How does the air in my city compare to Dhaka?"
+      maxlength="150"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import PromptInput from '@/components/PromptInput.vue';
 
-export default defineComponent({}); 
+enum View {
+  Greeting = 'GREETING',
+  Loading = 'LOADING'
+}
+
+export default defineComponent({
+  components: { PromptInput },
+  setup() {
+    const inputValue = ref('');
+    const view = ref(View.Greeting);
+
+    const onInputChange = (value: string) => {
+      inputValue.value = value;
+    };
+
+    const onSubmit = async () => {};
+
+    return { inputValue, onInputChange, onSubmit, view, View };
+  }
+});
 </script>
+
+<style scoped lang="scss">
+.main-container {
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+}
+</style>
